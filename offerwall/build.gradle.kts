71b69b521bac27cc9755dd101799aa9e3163b184
abhinav <abhinav@xaprio.com>
2024-03-24 17:49:22 +0530

    library module

--- offerwall/build.gradle.kts
+++ offerwall/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services")
+//    id("com.google.gms.google-services")
     id("kotlin-kapt")
     id("maven-publish")
 
